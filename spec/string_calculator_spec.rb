868026a018e0a27603fff6645d8c657ade8a9b54
Vaibhav Agrawal <vaibhavagr711@gmail.com>
2025-03-17 16:48:02 +0530

    Add failing test for different delimeters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -24,4 +24,10 @@ RSpec.describe 'StringCalculator' do
   it 'handle new lines between numbers' do
     expect(string_calculator.add("1\n2,3")).to eq(6)
   end
+
+  context 'supports different delimeters' do
+    it 'adds two values in custom delimeter string' do
+      expect(string_calculator.add("//;\n1;2")).to eq(3)
+    end
+  end
 end
